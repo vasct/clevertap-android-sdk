a79e626fea1798da50090054cdbdbda780e54107
Vassil Angelov <vassil@clevertap.com>
2025-04-23 23:22:32 +0300

    Remove redundant suppression
    Remove unused fields from CTInAppNotificationButton
    
    Set jacoco tools to 0.8.12
    
    Convert CTInAppNotificationButton to Kotlin
    
    Add tests for CTHmsMessageHandler.processPushAmp
    
    Add ReportGenerator to code coverage action
    
    Add Jacoco reporter with branch statistics
    
    Add MoranaApps/jacoco-report

--- clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/CTInAppNotificationButtonTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/CTInAppNotificationButtonTest.kt
@@ -1,34 +1,30 @@
 package com.clevertap.android.sdk.inapp
 
 import android.os.Parcel
-import com.clevertap.android.shared.test.BaseTestCase
+import com.clevertap.android.shared.test.RobolectricTest
 import org.json.JSONObject
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class CTInAppNotificationButtonTest: BaseTestCase() {
+class CTInAppNotificationButtonTest: RobolectricTest() {
 
     @Test
-    fun `initWithJson should parse json objects into field values`() {
-        val button = CTInAppNotificationButton()
-        val json = JSONObject(buttonJson)
-        button.initWithJSON(json)
+    fun `Constructor should parse json objects into field values`() {
+        val button = CTInAppNotificationButton(JSONObject(buttonJson))
 
         assertEquals(TEXT, button.text)
         assertEquals(TEXT_COLOR, button.textColor)
         assertEquals(BACKGROUND, button.backgroundColor)
         assertEquals(BORDER_COLOR, button.borderColor)
         assertEquals(BORDER_RADIUS, button.borderRadius)
-        assertTrue(button.action.type == InAppActionType.CLOSE)
+        assertTrue(button.action?.type == InAppActionType.CLOSE)
     }
 
     @Test
-    fun `objects should be correctly parceled`() {
+    fun `Objects should be correctly parceled`() {
         val parcel = Parcel.obtain()
-        val button = CTInAppNotificationButton()
-        val json = JSONObject(buttonJson)
-        button.initWithJSON(json)
+        val button = CTInAppNotificationButton(JSONObject(buttonJson))
 
         button.writeToParcel(parcel, 0)
         parcel.setDataPosition(0)
@@ -39,8 +35,7 @@ class CTInAppNotificationButtonTest: BaseTestCase() {
         assertEquals(button.backgroundColor, buttonFromParcel.backgroundColor)
         assertEquals(button.borderColor, buttonFromParcel.borderColor)
         assertEquals(button.borderRadius, buttonFromParcel.borderRadius)
-        assertEquals(button.action.type, buttonFromParcel.action.type)
-        assertEquals(button.error, buttonFromParcel.error)
+        assertEquals(button.action?.type, buttonFromParcel.action?.type)
     }
 
     companion object {
